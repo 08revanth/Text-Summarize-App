5d111b7453bcf9814b477c49291565e6c8051168
Revanth Malagi <145036676+08revanth@users.noreply.github.com>
2024-11-26 17:54:10 +0530

    Update summarize.js

--- summarize.js
+++ summarize.js
@@ -1,7 +1,6 @@
 // This is the function where the call to the API is made.
 // Returns the summarized text as a string.
 async function summarizeText(text) {
-    // INSERT CODE SNIPPET FROM POSTMAN BELOW
 const axios = require('axios');
 let data = JSON.stringify({
   "inputs": text,
