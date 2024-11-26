39a2496304dc9d05463a8a11582743652580ef2b
Revanth Malagi <145036676+08revanth@users.noreply.github.com>
2024-11-26 18:44:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -8,7 +9,8 @@ const summarizeText = require('./summarize.js');
 app.use(express.json());
 
 // Serves static files from the 'public' directory
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle POST requests to the '/summarize' endpoint
 app.post('/summarize', (req, res) => {
